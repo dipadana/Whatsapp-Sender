cacb34d040c94e77e47580c18b4307b612454780
dipadana <putudipadana@gmail.com>
2019-08-31 11:49:49 +0700

    80%

--- script.js
+++ script.js
@@ -2,18 +2,31 @@
 var telephone = document.getElementById('phone-number');
 var text = document.getElementById('text-message');
 
-// Merupakan variable untuk menampung tombol generate dan send
+// Merupakan variable untuk menampung tombol clear, generate dan send
 var generateBtn = document.getElementById('generate-link');
 var sendBtn = document.getElementById('send');
+var clearBtn = document.getElementById('clear-field');
 
 // Merupakan varibale penampung untuk display nomor tertuju
 var reseiverNumber = document.getElementById('receiver-number');
 
 generateBtn.addEventListener('click', function(){
   // Mengenerate API dari WA
-  document.getElementById('send').setAttribute('href',`https://wa.me/${telephone.value}?text=${text.value}`);
+  if(telephone.value && text.value){
+    sendBtn.setAttribute('href',`https://wa.me/${telephone.value}?text=${text.value}`);
+    sendBtn.removeAttribute('style','display: none');
+  }
 
   // Mengenerate nomor tertuju
   reseiverNumber.innerHTML = 'No Telephone = ' + telephone.value;
   console.log(sendBtn);
 })
+
+clearBtn.addEventListener('click', function(){
+  telephone.value = '';
+  text.value = '';
+  reseiverNumber.innerHTML = 'No Telephone = ';
+  sendBtn.removeAttribute('href');
+  sendBtn.setAttribute('style','display: none');
+})
+
